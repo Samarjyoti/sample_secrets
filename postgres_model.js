bcbf64fde447585e353f571f36a0d1b14359da6c
Samarjyoti Das <40276904+Samarjyoti@users.noreply.github.com>
2023-08-10 15:34:37 +0530

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -6,3 +6,8 @@ var pg_pass="sup3rstr0ngpass1ForGG";
 var pg_pass2="strongpassword";
 
 var mongo_uri = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority";
+
+var pg_port=1234;
+var pg_host="gitguardians.com:9082/BLUDBA";
+var pg_user="root1";
+var pg_pass="sup3rstr0ngpass1ForGG123";
