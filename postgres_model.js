8f3374af9f094bfa5e5aaa8bfb4af9f0cd2e82ec
Samarjyoti Das <40276904+Samarjyoti@users.noreply.github.com>
2023-08-10 15:36:44 +0530

    Update postgres_model.js

--- postgres_model.js
+++ postgres_model.js
@@ -11,3 +11,8 @@ var pg_port=1234;
 var pg_host="gitguardians.com:9082/BLUDBA";
 var pg_user="root1";
 var pg_pass="sup3rstr0ngpass1ForGG123";
+
+var pg_host="gitguardians.com:9082/BLUDB";
+var pg_user="root";
+var pg_pass="sup3rstr0ngpass1ForGG";
+var pg_pass2="strongpassword";
