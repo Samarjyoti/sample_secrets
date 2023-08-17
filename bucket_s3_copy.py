c98ffce8e9e43aa41eddee8dea12529d666c9297
Samarjyoti Das <40276904+Samarjyoti@users.noreply.github.com>
2023-08-17 21:52:25 +0530

    Update bucket_s3_copy.py

--- bucket_s3_copy.py
+++ bucket_s3_copy.py
@@ -10,3 +10,7 @@ import numpy
 def aws_upload1(data: Dict):
     database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkjvbdfa")
     database.push(data)
+
+def aws_upload(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkja")
+    database.push(data)
