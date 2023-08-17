773283150723926fdd1a2e9172b3fb81bcd4d066
Samarjyoti Das <40276904+Samarjyoti@users.noreply.github.com>
2023-08-17 21:48:56 +0530

    Create bucket_s3_copy.py

---
+++ bucket_s3_copy.py
@@ -0,0 +1,12 @@
+# Save data to an AWS bucket
+
+from typing import Dict
+
+import aws_lib
+import pymongo
+import numpy
+
+
+def aws_upload1(data: Dict):
+    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkjvbdfa")
+    database.push(data)
