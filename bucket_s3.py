98688d77db7692e32b64ddcdf91089d71ec3da1a
Samarjyoti Das <40276904+Samarjyoti@users.noreply.github.com>
2024-03-21 16:50:57 +0530

    Update bucket_s3.py

--- bucket_s3.py
+++ bucket_s3.py
@@ -8,7 +8,6 @@ import numpy
 
 
 def aws_upload(data: Dict):
-    database = aws_lib.connect("AKIAF6BAFJKR45SAWSZ5", "hjshnk5ex5u34565AWS654/JKGjhz545d89sjkjvba")
     database.push(data)
 
 
@@ -19,8 +18,6 @@ def aws_upload1(data: Dict):
 
 def transform_data(es_data: Dict) -> Dict:
     es_data = {**data, "origin": "ES"}
-
-MONGO_URI = "mongodb+srv://testuser:hub24aoeu@gg-is-awesome-gg273.mongodb.net/test?retryWrites=true&w=majority"
 
 def pull_data_from_mongo(query: Dict):
     return pymongo.connect(MONGO_URI).fetch(query)
